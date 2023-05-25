78714f57e7a0f355934b3dced5a60f839f42bf16
oreyato <oreyato.pro@gmail.com>
2023-05-25 16:41:56 +0200

    fix: glsl syntax errors

--- AdvancedOpenGL/assets/shaders/040_GameOfLife.glsl
+++ AdvancedOpenGL/assets/shaders/040_GameOfLife.glsl
@@ -6,31 +6,35 @@ layout (local_size_x = 32, local_size_y = 32) in;
 layout (binding = 0) uniform bool grid[32][32];
 */
 
-layout (std430, binding = 0) readonly buffer grid_in
+layout (std430, binding = 0) buffer grid_in
 {
-    cell cell[];
+    bool cell[];
 } input_data;
 
 layout (std430, binding = 1) buffer grid_out
 {
-    cell cell[];
+    bool cell[];
 } output_data;
 
-bool getNeighborStatus(vec2 offset) {
-    vec2 local_id = (int(gl_LocalInvocationID.x), int(gl_LocalInvocationID.y));
-    vec2 neighbour_id = local_id + offset;
+bool getNeighborStatus(const ivec2 local_idP, ivec2 offset) {
+    ivec2 neighbour_id = local_idP + offset;
     
     int gridIndex = neighbour_id.x + neighbour_id.y * 32;
 
-    return grid_in[gridIndex];
+    return input_data.cell[gridIndex];
 }
 
 void main() {
+    // Get local id
+    const ivec2 local_id = ivec2(gl_LocalInvocationID.x, gl_LocalInvocationID.y);
+    // Get local index
+    const int localIndex = local_id.x + local_id.y * 32;
+
     // Determine how much living neighbors there is
     int sum = 0;
     for (int x = -1; x <= 1; x++) {
         for (int y = -1; y <= 1; y++) {
-            sum += getNeighborStatus(vec2(x, y));
+            sum += int(getNeighborStatus(local_id, ivec2(x, y)));
         }
     }
 
@@ -38,10 +42,10 @@ void main() {
     bool newStatus = false;
     if (sum == 3) {
         newStatus = true;
+    } else if (sum == 2) {
+        newStatus = input_data.cell[localIndex];
     }
     
     // Set changes to grid_out
-    vec2 local_id = (int(gl_LocalInvocationID.x), int(gl_LocalInvocationID.y));
-    int localIndex = local_id.x + local_id.y * 32;
-    grid_out[localIndex] = newStatus;
+    output_data.cell[localIndex] = newStatus;
 }
